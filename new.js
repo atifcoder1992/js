6dd844d2436187a70a1334fd56ee406fd2160d0a
atifcoder1992 <73212092+atifcoder1992@users.noreply.github.com>
2021-10-17 21:37:24 +0200

    Add files via upload

---
+++ new.js
@@ -0,0 +1,24 @@
+
+ 
+
+document.getElementById("text").innerHTML = name;
+
+
+var name =prompt("الاسم" , "عاطف ابكر عبدالوهاب محمد ابراهيم");
+alert(name)
+
+var addres =prompt("السكن" , "الخرطوم_مايو");
+alert(addres)
+
+var age =prompt("العمر" , "29 سنه");
+
+
+var college =prompt("الجامعه " , "كليه الامارات للعلوم والتكنولوجيا");
+window.alert(college + age);
+
+window. close();
+
+
+
+
+
